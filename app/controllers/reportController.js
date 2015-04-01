c84b46a6fcb5685ece5c0f2fe41eedd765617740
Suren <19suren89@gmail.com>
2015-04-01 19:01:56 +0400

    Creating one-window output

--- app/controllers/reportController.js
+++ app/controllers/reportController.js
@@ -5,10 +5,14 @@
         $scope.test = "Test Variable";
         $scope.boxName = "";
         $scope.boxFlag = false;
+        $scope.output = [];
 
         $scope.openBox = function(selected) {
-            if ($scope.selected.label === "Done") {
+            if ($scope.selected.label !== "") {
                 $scope.boxFlag = true;
+                $scope.output.push({
+                                    heading: $scope.selected.label
+                                  });
             } else {
                 $scope.boxFlag = false;
             }
@@ -25,11 +29,25 @@
             }
         };
 
+        $scope.addToHeading = function(areaText, label) {
+            console.log("______aaaaaaaa________:" + areaText);
+            console.log("and label is: " + label + "::::::::");
+            for (var key in $scope.output) {
+                if ($scope.output[key].heading === label) {
+                    $scope.output[key].text = areaText;
+                }
+            }
+
+        }
+
 
         $scope.options = [
             { label: 'Done'},
-            { label: 'To-Do'}
+            { label: 'To-Do'},
+            { label: 'Skipped'}
         ];
+
+
 
 
 
--- app/directives/selectAndBox.js
+++ app/directives/selectAndBox.js
@@ -2,9 +2,16 @@
 
 angular.module('reportApp').directive('selectAndBox', function() {
     return {
-        restrict: 'E', 
-        templateUrl: "app/views/selectAndBox.html"
-    }
+        restrict: 'AE', 
+        templateUrl: "app/views/selectAndBox.html",
+        link: function(scope, elem, attrs) {
+            
+            console.log('scope is: ', scope);
+            console.log('elem is: ', elem);
+            console.log('attrs is: ', attrs);
+
+            }
+        }
 });
 
 
